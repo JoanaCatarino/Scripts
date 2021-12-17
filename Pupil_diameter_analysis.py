78d967fef6e5ae3caf6633a10abc16bfe8e3fdd0
JoanaCatarino <91877134+JoanaCatarino@users.noreply.github.com>
2021-12-17 18:55:37 +0000

    Add files via upload

--- Pupil_diameter_analysis.py
+++ Pupil_diameter_analysis.py
@@ -50,21 +50,36 @@ for i, eid in enumerate(eids):
 
     # Choose event to plot 
     
-    
-    
-    
-    #example
-    # 0) create a string wiht only the data you want (e.g. correct)
-    # 1) define variable for the pre- and post-event
-    #pre = x sec
-    #post = x sec
-    #2)cut data around the event 
-    # reward (i,:) = 
+    f, ax2 = plt.subplots(1, 1, dpi=500)
     
 
-#%%
+    
+
+#%% Random stuff 
+
     # Table for incorrect trials   
-    df_IncorrectTrials = df_Trials[['feedbackType']==-1]  
+  IncorrectTrials = df_Trials['feedbackType']==-1
+    
+  # Get trial triggered baseline subtracted pupil diameter (Guido's script)
+        for t, trial_start in enumerate(trials['stimOn_times']):
+            this_diameter = np.array([np.nan] * TIME_BINS.shape[0])
+            baseline_subtracted = np.array([np.nan] * TIME_BINS.shape[0])
+            baseline = np.nanmedian(diameter_perc[(video_times > (trial_start - BASELINE[0]))
+                                                  & (video_times < (trial_start - BASELINE[1]))])
+            for b, time_bin in enumerate(TIME_BINS):
+                this_diameter[b] = np.nanmedian(diameter_perc[
+                    (video_times > (trial_start + time_bin) - (BIN_SIZE / 2))
+                    & (video_times < (trial_start + time_bin) + (BIN_SIZE / 2))])
+                baseline_subtracted[b] = np.nanmedian(diameter_perc[
+                    (video_times > (trial_start + time_bin) - (BIN_SIZE / 2))
+                    & (video_times < (trial_start + time_bin) + (BIN_SIZE / 2))]) - baseline
+            pupil_size = pupil_size.append(pd.DataFrame(data={
+                'diameter': this_diameter, 'baseline_subtracted': baseline_subtracted, 'eid': eid,
+                'subject': nickname, 'trial': t, 'contrast': trials.loc[t, 'signed_contrast'],
+                'sert': subjects.loc[i, 'sert-cre'], 'laser': trials.loc[t, 'laser_stimulation'],
+                'laser_prob': trials.loc[t, 'laser_probability'], 'laser_block_progress': trials.loc[t, 'block_progress'],
+                'time': TIME_BINS}))
+
     
 
 
